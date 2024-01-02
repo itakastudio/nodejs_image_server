f65ac13d5b98ee4057dc85208889c7c0104d4a0d
Chin Ching Au-Yeung <chinchingau-yeung@Oscar-Mac-mini.local>
2024-01-02 14:11:33 +0800

    Upload source code

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
-const http = require('http');
-const PORT = 3000;
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const https = require('https');
+const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+const app = express();
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+app.use('/img', express.static('img'));
+
+app.get('/', async (req, res) => {
+    const imageUrl = req.query.img;
+    const imageName = `./img/${Date.now()}_${getRandomInt(10)}.jpg`;
+    const file = fs.createWriteStream(imageName);
+
+    console.log(imageUrl);
+
+    https.get(imageUrl, response => {
+        response.pipe(file);
+
+        file.on('finish', () => {
+            const resultUrl = `${req.protocol}://${req.get('host')}${imageName.slice(1)}`;
+            console.log(`Image stored at ${resultUrl}`);
+            res.status(200).send(resultUrl);
+            file.close();
+        });
+    }).on('error', err => {
+        console.error(`Error downloading image: ${err.message}`);
+        res.status(400).send(`Error downloading image: ${err.message}`);
+        fs.unlink(imageName);
+    });
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+app.listen(process.env.PORT, () => {
+    console.log(`Now listening on port ${process.env.PORT}`);
 });
